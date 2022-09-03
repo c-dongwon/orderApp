8dce19089d8e8d3f98525308fda343e5818e5144
c-dongwon <clzlqndqnd1@gmail.com>
2022-09-03 18:45:39 +0900

    [new-Ing]{엑세스토큰 재발급} { 1.엑세스토큰 재발급 작업 완료 }

--- src/slices/user.ts
+++ src/slices/user.ts
@@ -24,6 +24,9 @@ const userSlice = createSlice({
         setMoney(state, action: PayloadAction<number>) {
             state.money = action.payload;
         },
+        setAccessToken(state, action) {
+            state.accessToken = action.payload;
+        },
     },
     extraReducers: (builder) => {},
 });
